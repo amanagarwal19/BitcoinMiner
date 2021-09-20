ba1c881e3ccb0d784dfa8ef3d4b0579f00c9407b
amanagarwal19 <90938481+amanagarwal19@users.noreply.github.com>
2021-09-20 19:32:35 -0400

    Add files via upload
    Remote clent

---
+++ actorClient.fsx
@@ -0,0 +1,179 @@
+#r "nuget: Akka.FSharp"
+#r "nuget: Akka.Remote"
+#load "Util.fsx"
+
+open System
+open System.Threading
+open Akka.Actor
+open Akka.Configuration
+open Akka.FSharp
+open Util
+
+let zeros = fsi.CommandLineArgs.[1]|>int;
+let workerCount = 5
+let totalStrings = 10000000
+let eachSideDuty = totalStrings/2
+let eachWorkerEffort = eachSideDuty / workerCount
+// printfn "Worker effort : %d" eachWorkerEffort
+let mutable clientSideCompleted = false;
+let mutable serverSideCompleted = false;
+// To keep a count of when all the workers have finished and terminate the program
+let mutable workersFinished = 0
+
+let clientConfiguration = 
+    ConfigurationFactory.ParseString(   
+        @"akka{
+            actor.provider = ""Akka.Remote.RemoteActorRefProvider, Akka.Remote""    
+            remote{
+                helios.tcp{
+                    port:8202
+                    hostname:localhost
+                }
+            }
+        }")
+
+let system = System.create "Client" clientConfiguration
+
+
+//Initializing message types
+type MessageFormat = 
+    |CreateWorkers of int
+    |ReceiveMessageFromWorker of int*string*string
+    |StartYourJob of int*int*int
+    |Finished of int*string
+    // |Finish of string
+
+//Creating working actors
+let worker (mailbox:Actor<_>)=
+    let rec loop() = actor{
+        let! msg = mailbox.Receive()
+        let sender = mailbox.Sender()
+
+        match msg with
+
+        |StartYourJob(ID,capacity,startCharOffset) -> 
+            // printfn "Worker %d Initialised\n" ID
+
+            //String to store all the encrypted strings and their corresponding keys
+            let mutable str = ""
+            let mutable invalidCounter = 0
+            let mutable validCounter = 0
+            //Perform the encryption
+            for i = 1 to capacity do
+                let stringToEncrypt = stringGenerator(startCharOffset)
+                let encryptedString = encrypt(stringToEncrypt)
+
+                //Check for validation of the encryption string
+                if validCoin(encryptedString,zeros) then 
+                    sender<!ReceiveMessageFromWorker(ID,stringToEncrypt,encryptedString)
+
+            sender<!Finished(ID,"Done");
+
+        | _ -> printfn "Incorrect message"
+
+        return! loop()
+    }
+    loop()
+
+
+//Creating the boss
+
+let bossActor (mailbox:Actor<_> ) = 
+    let rec loop() = actor{
+        let! msg = mailbox.Receive()
+        
+        let mutable workers =[]
+        
+        match msg with 
+        |CreateWorkers (n)->
+            workers <- [for i in 0..(workerCount-1) do 
+                            yield spawn system ("Worker" + (string i)) worker]
+                       
+            // printfn "\nTotal number of workers %d \n" workers.Length
+
+            let mutable characterStartOffset =0
+            
+            //Commanding the workers to start their jobs
+            let mutable workerID = 1
+            for i in 0..(workerCount-1) do //minus 1 because zero index based
+                
+                workers.Item(i|>int)<!StartYourJob(workerID,eachWorkerEffort,characterStartOffset)
+                characterStartOffset<- characterStartOffset + 5
+                workerID <- workerID + 1
+            
+            // printfn "welcome to assigning task to %d people" n
+
+        |ReceiveMessageFromWorker (workerID,generatedString,encryptedString) ->
+            printfn "WorkerID : %d \nInput: %s \nOutput: %s" workerID generatedString encryptedString
+            
+        |Finished(workerID,message)->
+            // printfn "Received %s \nOkay, good job worker %d" message workerID
+            
+            //Everyone has finished processing, terminate the process
+            workersFinished <- workersFinished + 1
+            // printfn "finished workers %d" workersFinished
+            if workersFinished = workerCount then 
+                // mailbox.Context.System.Terminate() |>ignore
+                clientSideCompleted <- true
+                printfn "Client side has finished thier job"
+
+        | _ -> printfn "Incorrect message"
+        
+
+        return! loop()
+    }
+    loop()
+
+let clientBoss = spawn system "bossActor" bossActor
+
+
+
+
+
+
+
+// -----------------------Connecting to remote actor system----------------------------
+
+// let serverIp = fsi.CommandLineArgs.[2]
+// let serverPort = fsi.CommandLineArgs.[3]
+
+let serverAddress = "192.168.251.176"
+let serverPort = "8201"
+
+let connectionAddress = "akka.tcp://RemoteSystem@" + serverAddress + ":" + serverPort + "/user/server" 
+printfn "%s" connectionAddress
+
+// Creating a client actor
+let client (mailbox:Actor<_>) = 
+    let rec loop() = actor{
+        let! message = mailbox.Receive()
+        let sender = mailbox.Sender()
+        let incoming = message|>string
+        
+        if incoming = "godspeed" then
+
+            //Assign task for remote actor
+            let remoteActor = system.ActorSelection(connectionAddress)
+            let taskForRemoteActor = "Perform "+(string eachSideDuty)+","+ (string zeros) + "zeros"
+            remoteActor<!taskForRemoteActor
+
+            //Assign task for local actors
+            clientBoss<!CreateWorkers(workerCount)
+
+        else if incoming = "ServerSide Completed" then 
+                serverSideCompleted <- true
+                system.Terminate() |> ignore
+        else
+            printfn "default: %s " incoming
+
+        return! loop()
+    }
+    loop()
+
+let clientRef = spawn system "client" client
+
+clientRef<!"godspeed"
+
+while (not clientSideCompleted && not serverSideCompleted) do
+
+system.WhenTerminated.Wait()
