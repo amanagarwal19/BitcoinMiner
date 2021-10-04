79bf5d1f24674712263abb8d0e3f26b405df3c7b
amanagarwal19 <90938481+amanagarwal19@users.noreply.github.com>
2021-10-04 01:32:02 -0400

    Delete Bitcoin directory

--- Bitcoin/ifelse.fs
+++
@@ -1,9 +0,0 @@
-open System
-
-[<EntryPoint>]
-let main argv =
-    printfn "hello  from if else"
-    let age = Console.ReadLine()
-    let citizenType = if int age >50 then "Senior citizen" elif int age >30 then "Citizen" else "Child"
-    printfn "%s" citizenType
-    0
